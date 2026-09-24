a2a6c008664b35d1ca3a36e6272d67ee6b33d708
Developer <dev@local.com>
2026-09-24 12:30:21 +0300

    Fix: Enable node js-runtime and android client for cloud downloading

--- server.js
+++ server.js
@@ -154,7 +154,8 @@ app.get('/api/info', async (req, res) => {
     '--no-warnings',
     '--no-playlist',
     '--skip-download',
-    '--extractor-args', 'youtube:player_client=android,ios,web',
+    '--js-runtimes', 'node',
+    '--extractor-args', 'youtube:player_client=android',
     cleanUrl
   ];
 
@@ -245,7 +246,8 @@ app.post('/api/convert', (req, res) => {
     '--no-warnings',
     '--no-playlist',
     '--newline',
-    '--extractor-args', 'youtube:player_client=android,ios,web'
+    '--js-runtimes', 'node',
+    '--extractor-args', 'youtube:player_client=android'
   ];
 
   if (isAudio) {
